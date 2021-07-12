a027b8bb2125756183c6024bb97309070a4186c1
Fekz115 <fekz115@gmail.com>
2021-07-12 18:54:37 +0300

    feat(add screens): initial implementation of main screen

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio_flutter/data.dart';
+import 'package:portfolio_flutter/widgets/info_widget.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background/main_screen_background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0x00bdbdbd),
+                    Color(0xDDbdbdbd),
+                    Color(0xDDbdbdbd),
+                  ],
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              padding: const EdgeInsets.all(30),
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    InfoWidget(
+                      personInfo: personInfo,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
