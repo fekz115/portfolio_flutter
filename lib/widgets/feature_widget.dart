8adf7ec9f7c45b698f45b6e621fe360b79659d4d
Fekz115 <fekz115@gmail.com>
2021-09-04 18:00:59 +0300

    feat(widgets): feature widget

---
+++ lib/widgets/feature_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class FeatureWidget extends StatelessWidget {
+  const FeatureWidget({
+    Key? key,
+    required this.value,
+  }) : super(key: key);
+
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Icon(
+          Icons.star_border,
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        Flexible(
+          child: Text(value),
+        ),
+      ],
+    );
+  }
+}
