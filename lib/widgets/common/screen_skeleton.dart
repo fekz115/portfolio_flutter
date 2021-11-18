a76609b5c7cf99794c23aa6ccfdf5ba817933603
Fekz115 <fekz115@gmail.com>
2021-11-18 17:10:34 +0300

    feat(ui): appearance animation for screen background

--- lib/widgets/common/screen_skeleton.dart
+++ lib/widgets/common/screen_skeleton.dart
@@ -37,6 +37,22 @@ class ScreenSkeleton extends StatelessWidget {
                   child: Image.asset(
                     style.background,
                     fit: BoxFit.fitWidth,
+                    frameBuilder: (
+                      BuildContext context,
+                      Widget child,
+                      int? frame,
+                      bool wasSynchronouslyLoaded,
+                    ) {
+                      if (wasSynchronouslyLoaded) {
+                        return child;
+                      }
+                      return AnimatedOpacity(
+                        opacity: frame == null ? 0 : 1,
+                        duration: const Duration(seconds: 1),
+                        curve: Curves.easeOut,
+                        child: child,
+                      );
+                    },
                   ),
                 ),
                 Positioned.fill(
